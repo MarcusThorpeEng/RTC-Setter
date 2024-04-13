0f44142bf826f0b5cfffa45c604a82a36501cae3
MarcusThorpe2000 <mdthorpe1@sheffield.ac.uk>
2024-04-13 18:53:49 +0100

    Finished with Fixed time upload, only rewrites time if battery disconect flagged

--- src/main.cpp
+++ src/main.cpp
@@ -6,19 +6,20 @@
 RTC_DS3231 rtc;
 
 void setup() {
+
   Serial.begin(9600);
   
   // Check if the RTC is connected properly
   if (!rtc.begin()) {
     Serial.println("Couldn't find RTC");
-    while (1);
+    //while (1);
   }
 
   // Check if the RTC lost power and if so, set the time - this will run on first tiem
   if (rtc.lostPower()) {
     Serial.println("RTC lost power, let's set the time!");
-    // Set the RTC to a specific date and time
-    // rtc.adjust(DateTime(year, month, day, hour, minute, second));
+    //Set the RTC to a specific date and time
+     //rtc.adjust(DateTime(year, month, day, hour, minute, second));
     rtc.adjust(DateTime(2024, 1, 1, 12, 0, 0)); // January 1, 2024, 12:00:00
   }
 }
