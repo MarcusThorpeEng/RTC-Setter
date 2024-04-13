d9a45d625a7c0de7c581686b413e2d8d6bb6cc2d
MarcusThorpe2000 <mdthorpe1@sheffield.ac.uk>
2024-04-13 19:02:11 +0100

    Removed Previous comments, Fixed compile time to 6s

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ using namespace CompileTime;
 RTC_DS3231 rtc;
 
 void setup() {
-  CompileTime::setCompileTime(7.99); // pass the number of seconds it takes to upload
+  CompileTime::setCompileTime(6); // pass the number of seconds it takes to upload
   Serial.begin(9600);
   
   // Check if the RTC is connected properly
@@ -46,33 +46,3 @@ void loop() {
   delay(1000);
 }
 
-
-// /*
-//  * CompileTime.ino
-//  * 
-//  * example Arduino IDE sketch for the CompileTime library
-//  * 
-//  * version 1.0 written June 2023 - Trent M. Wyatt
-//  * 
-//  */
-// #include "CompileTime.h"
-
-// using namespace CompileTime;
-
-// void setup() {
-//     CompileTime::setCompileTime(6); // pass the number of seconds it takes to upload
-//     Serial.begin(115200);
-// }
-
-// void loop() {
-//     static int16_t lasth = hour, lastm = minute, lasts = second;
-//     updateTime(micros());
-
-//     if (lasts != second || lastm != minute || lasth != hour) {
-//         lasts  = second;   lastm  = minute;   lasth  = hour;
-
-//         char buff[16];
-//         sprintf(buff, "%s%d %d - %2d:%02d:%02d", month, day, year, hour, minute, second);
-//         Serial.println(buff);
-//     }
-// // }
